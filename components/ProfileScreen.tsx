0a67b217332cd10c5b04c57d0b1964cc47b8626b
weepha <bankboom0884680216@gmail.com>
2024-07-27 11:35:19 +0700

    Practice2_ch04_ReactHook

--- components/ProfileScreen.tsx
+++ components/ProfileScreen.tsx
@@ -1,18 +1,32 @@
-import { StyleSheet, Text, View, Image } from "react-native";
-import React from "react";
+import { Text, View, Image, Button } from "react-native";
+import React, { useState } from "react";
 import styles from "../styles/styles";
 
 const ProfileScreen = (): React.JSX.Element => {
-  const ProfileImage = require("../assets/รูปถ่าย3.jpg");
+  const ProfileImage = require("../assets/รูปถ่าย.jpg");
+  const ProfileImage1 = require("../assets/รูปถ่าย3.jpg");
+  const [name, setName] = useState("Weerawat Phapukdee");
+  const [image, setImage] = useState(ProfileImage);
+
+  const handChangeName =() => {
+    setName("Weewy");
+  }
+  const handChangeImage =()=> {
+    setImage(ProfileImage1)
+  }
   return (
     <View style={styles.container}>
       <View style={styles.profileContainer}>
-        <Image source={ProfileImage} style={styles.profileImage} />
-        <Text style={styles.profileName}>Weerawat Phapukdee</Text>
+        <Image source={image} style={styles.profileImage} />
+        <View>
+          <Text style={styles.profileName}>{name}</Text>
+          <Button title="change name" onPress={handChangeName} />
+          <Text>{"\n"}</Text>
+          <Button title="change image" onPress={handChangeImage} />
+        </View>
       </View>
     </View>
   );
 };
 
 export default ProfileScreen;
-
